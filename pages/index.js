bc5257efd3b854b091722044f61b7b29ad4a4b47
jeromehaas <hello@jeromehaas.ch>
2023-02-08 16:27:12 +0100

    Setup: heading component

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
+import Heading from "components/01-atoms/heading/heading";
+
 const Start = () => {
 
   return (
     <div className="start">
-      Hello World!
+      <Heading className="start__heading" level="h1">Heading H1</Heading>
+      <Heading className="start__heading" level="h2">Heading H2</Heading>
+      <Heading className="start__heading" level="h3">Heading H3</Heading>
+      <Heading className="start__heading" level="h4">Heading H4</Heading>
     </div>
   );
 
