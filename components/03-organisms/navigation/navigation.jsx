1c2f98a32f1d0eaa077417ea13c9bdb598f65647
jeromehaas <hello@jeromehaas.ch>
2023-02-10 18:42:55 +0100

    Setup: navigation component

---
+++ components/03-organisms/navigation/navigation.jsx
@@ -0,0 +1,66 @@
+import Hamburger from "components/01-atoms/hamburger/hamburger";
+import Image from "next/image";
+import Link from "next/link";
+import { useState, useRef, useEffect } from 'react';
+import { gsap } from 'gsap';
+
+const Navigation = () => {
+
+    const [ isOpen, setIsOpen ] = useState(false);
+    const navigationRef = useRef();
+    const timelineRef = useRef();
+
+    const toggleMenu = () => {
+        isOpen ? closeMenu() : openMenu();
+        setIsOpen(!isOpen);
+    };
+
+    const openMenu = () => {
+        timelineRef.current.play();
+    };
+
+    const closeMenu = () => {
+        timelineRef.current.reverse();
+    };
+
+    useEffect(() => {
+        gsap.context(() => {
+            timelineRef.current = gsap.timeline();
+            timelineRef.current.to('.navigation .navigation__menu', { bottom: '0%', duration: 1, ease: 'power4.inOut' }).reverse();
+            timelineRef.current.to('.navigation .main-links__item', { opacity: '1', duration: 0.5, ease: 'power4.inOut', stagger: { amount: 0.5 } }).reverse();
+            timelineRef.current.to('.navigation .social-links__item', { opacity: '1', duration: 0.5, ease: 'power4.inOut' }).reverse();
+        }, navigationRef);
+        () => timeline.current.kill();
+    }, []);
+
+    return (
+        <nav className="navigation" ref={ navigationRef }>
+            <div className="navigation__inner">
+                <div className="navigation__placeholder"></div>
+                <div className="navigation__background"></div>
+                <div className="navigation__bar bar">
+                    <Link className="bar__logo-link" href="/">
+                        <Image className="bar__logo" src="/logos/samira-haas.svg" alt="Samira Haas" width="140" height="40" priority />
+                    </Link>
+                    <Hamburger className="bar__hamburger" isOpen={ isOpen } onClick={ toggleMenu } /> 
+                </div>
+                <div className="navigation__menu menu">
+                     <div className="menu__main-links main-links">
+                        <Link className="main-links__item heading--h1" href="/corporate">Corporate</Link>
+                        <Link className="main-links__item heading--h1" href="/corporate">Storytelling</Link>
+                        <Link className="main-links__item heading--h1" href="/corporate">Architektur</Link>
+                        <Link className="main-links__item heading--h1" href="/corporate">Hochzeiten</Link>
+                        <Link className="main-links__item heading--h1" href="/corporate">Gestaltung</Link>
+                        <Link className="main-links__item heading--h1" href="/corporate">Kontakt</Link>
+                     </div>
+                     <div className="menu__social-links social-links">
+                        <Link className="social-links__item" href="/">Instagram</Link>
+                     </div>
+                </div>
+            </div>
+        </nav>
+    );
+
+};
+
+export default Navigation;
--- components/04-layouts/page/page.jsx
+++ components/04-layouts/page/page.jsx
@@ -1,11 +1,13 @@
 import PropTypes from 'prop-types';
 import Head from 'components/01-atoms/head/head';
+import Navigation from 'components/03-organisms/navigation/navigation';
 
 const Page = ({ className, children, meta }) => {
 
     return (
         <div className={`${ className } page`}>
             <Head meta={ meta } />
+            <Navigation />
             { children }
         </div>
     );
