2e59ca5232e450258416fc873f217039fe3dc4ac
jeromehaas <hello@jeromehaas.ch>
2023-02-25 14:12:30 +0100

    Setup: behind-the-scenes component

---
+++ frontend/components/03-organisms/behind-the-scenes/behind-the-scenes.jsx
@@ -0,0 +1,14 @@
+import Section from "components/04-layouts/section/section";
+import PictureCollection from "components/02-molecules/picture-collection/picture-collection";
+
+const BehindTheScenes = () => {
+    
+    return(
+        <Section className="behind-the-scenes">
+            <PictureCollection />
+        </Section>
+    )
+
+};
+
+export default BehindTheScenes;
--- frontend/pages/contact.js
+++ frontend/pages/contact.js
@@ -1,6 +1,7 @@
 import Page from "components/04-layouts/page/page";
 import SayHello from "components/03-organisms/say-hello/say-hello";
 import Profile from "components/03-organisms/profile/profile";
+import BehindTheScenes from "components/03-organisms/behind-the-scenes/behind-the-scenes";
 
 const Contact = () => {
 
@@ -8,6 +9,7 @@ const Contact = () => {
         <Page>
             <SayHello />
             <Profile />
+            <BehindTheScenes />
         </Page>
     )
 
