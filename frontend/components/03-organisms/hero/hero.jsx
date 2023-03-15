c2436ad4f2ab8915e6452dc7fbd340a0b85d2015
jeromehaas <hello@jeromehaas.ch>
2023-03-15 18:13:48 +0100

    Setup: animations for hero component

--- frontend/components/03-organisms/hero/hero.jsx
+++ frontend/components/03-organisms/hero/hero.jsx
@@ -4,8 +4,24 @@ import Text from 'components/01-atoms/text/text';
 import Picture from 'components/01-atoms/picture/picture';
 import Section from 'components/04-layouts/section/section';
 import Scroller from 'components/01-atoms/scroller/scroller';
+import { useRef, useEffect } from 'react';
+import { gsap } from 'gsap';
 
 const Hero = () => {
+
+    const heroRef = useRef();
+    const heroTimelineRef = useRef();
+
+    useEffect(() => {
+        gsap.context(() => {
+            heroTimelineRef.current = gsap.timeline();
+            heroTimelineRef.current.to('.hero__heading', { autoAlpha: 1, duration: 2, delay: 1 } )
+            heroTimelineRef.current.to('.hero__text', { autoAlpha: 1, duration: 1 }, '-=1' )
+            heroTimelineRef.current.to('.hero__scroller', { autoAlpha: 1, duration: 1 }, '-=0.5' )
+        }, heroRef.current);
+        () =>  heroTimelineRef.current.kill();
+    }, []);
+
 
     return (
         <Section className="hero">
