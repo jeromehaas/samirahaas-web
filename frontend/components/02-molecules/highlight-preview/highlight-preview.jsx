58fc4da1fc65d98b2655e39b9d852153d0851c12
jeromehaas <hello@jeromehaas.ch>
2023-05-08 17:01:31 +0200

    Refactor: add link to gallery items

--- frontend/components/02-molecules/highlight-preview/highlight-preview.jsx
+++ frontend/components/02-molecules/highlight-preview/highlight-preview.jsx
@@ -3,9 +3,10 @@ import { gsap } from 'gsap';
 import Heading from 'components/01-atoms/heading/heading';
 import PropTypes from 'prop-types';
 import Picture from 'components/01-atoms/picture/picture';
+import Link from 'next/link';
 
 const HighlightPreview = ({ className, items }) => {
-    
+
     const timeline = useRef();
     const previewRef = useRef();
     const [ counter, setCounter ] = useState(0);
@@ -14,27 +15,29 @@ const HighlightPreview = ({ className, items }) => {
         const isLastSlide = counter === items.length - 1;
         const newCounter = isLastSlide ? 0 : counter + 1;
         setCounter(newCounter);
-    }
-
-    const playSlide = () => {
-        gsap.context(() => {
-            const status = gsap.utils.toArray('.progress .progress__status');
-            const images = gsap.utils.toArray('.preview .preview__image');
-            const boxes = gsap.utils.toArray('.projects .box');
-            timeline.current = gsap.timeline({ onComplete: updateCounter });
-            timeline.current.to(images, { opacity: 0, duration: 0.25, ease: 'inOut' }, 0);
-            timeline.current.to(boxes, { opacity: 0, duration: 0.25, ease: 'inOut' }, 0);
-            timeline.current.to(status, { right: '100%', left: '0%', duration: 0 }, 0);
-            timeline.current.to(boxes[counter], { opacity: 1, duration: 2, ease: 'inOut' }, 0.25);
-            timeline.current.to(images[counter], { opacity: 1, duration: 2, ease: 'inOut' }, 0.25);
-            timeline.current.to(status[0], { right: '0%', duration: 2, ease: 'none' });
-            timeline.current.to(status[0], { left: '100%', duration: 2, ease: 'none ' });
-            timeline.current.to(images[counter], { opacity: 0, duration: 0.25, ease: 'inOut' });
-        }, previewRef );
     };
 
     useEffect(() => {
-        playSlide();
+			const context = gsap.context(() => {
+					const status = gsap.utils.toArray('.progress .progress__status');
+					const images = gsap.utils.toArray('.preview .preview__image');
+					const boxes = gsap.utils.toArray('.projects .box');
+					timeline.current = gsap.timeline({ onComplete: updateCounter });
+					timeline.current.to(status, { right: '100%', left: '0%', duration: 0 }, 0);
+					timeline.current.to(images, { opacity: 0, duration: 2, ease: 'inOut' }, 0);
+					timeline.current.to(boxes, { zIndex: 0, duration: 0, ease: 'inOut' }, 0);
+					timeline.current.to(boxes[counter], { zIndex: 5, duration: 0, ease: 'inOut' }, 0);
+					timeline.current.to(boxes, { opacity: 0, duration: 2, ease: 'inOut' }, 0);
+					timeline.current.to(boxes[counter], { opacity: 1, duration: 2, ease: 'inOut' }, 0.5);
+					timeline.current.to(images[counter], { opacity: 1, duration: 2, ease: 'inOut' }, 0.5);
+					timeline.current.to(status[0], { opacity: 1, duration: 2, ease: 'none ' }, 0.5);
+					timeline.current.to(status[0], { right: '0%', duration: 6, ease: 'none' }, 2.5);
+					timeline.current.to(status[0], { opacity: 0, duration: 2, ease: 'inOut' }, 10);
+					timeline.current.to(images[counter], { opacity: 0, duration: 2, ease: 'inOut' }, 10);
+					timeline.current.to(images[counter], { opacity: 0, duration: 2, ease: 'inOut' }, 10);
+					timeline.current.to(boxes[counter], { opacity: 0, duration: 2, ease: 'inOut' }, 10);
+			}, previewRef);
+			return () => context.revert();
     }, [counter])
     
     return (
@@ -50,10 +53,10 @@ const HighlightPreview = ({ className, items }) => {
             </div>
             <div className="highlight-preview__projects projects">
                 { items.map((item) => (
-                    <div className="projects__box box" key={ item.id }>
+                    <Link className="projects__box box" key={ item.id } href={`/project/${ item.id }`}>
                         <Heading className="box__label" level="h3">{ item.attributes.category }</Heading>
                         <Heading className="box__title" level="h5" >{ item.attributes.heading }</Heading>
-                    </div>
+                    </Link>
                 ))}
             </div>
         </div>
--- frontend/components/02-molecules/highlight-slider/highlight-slider.jsx
+++ frontend/components/02-molecules/highlight-slider/highlight-slider.jsx
@@ -3,6 +3,7 @@ import { gsap } from 'gsap';
 import Heading from 'components/01-atoms/heading/heading';
 import PropTypes from 'prop-types';
 import Picture from 'components/01-atoms/picture/picture';
+import Link from 'next/link';
 
 const HighlightSlider = ({ className, items }) => {
 
@@ -14,34 +15,35 @@ const HighlightSlider = ({ className, items }) => {
         const isLastSlide = counter === items.length - 1;
         const newCounter = isLastSlide ? 0 : counter + 1;
         setCounter(newCounter);
-    }
-
-    const playSlide = () => {
-        const context = gsap.context(() => {
-            const status = gsap.utils.toArray('.box .progress__status');
-            const images = gsap.utils.toArray('.preview .preview__image');
-            const labels = gsap.utils.toArray('.box .box__label');
-            const titles = gsap.utils.toArray('.box .box__title');
-            timeline.current = gsap.timeline({ onComplete: updateCounter });
-            timeline.current.to(status, { right: '100%', left: '0%', duration: 0, ease: 'inOut' }, 0);
-            timeline.current.to(images, { opacity: 0, duration: 0.5, ease: 'inOut' }, 0);
-            timeline.current.to(labels, { opacity: 0.2, duration: 0.5, ease: 'inOut' }, 0);
-            timeline.current.to(titles, { opacity: 0.2, duration: 0.5, ease: 'inOut' }, 0);
-            timeline.current.to(labels[counter], { opacity: 1, duration: 0.5, ease: 'inOut' }, 0.5);
-            timeline.current.to(titles[counter], { opacity: 1, duration: 0.5, ease: 'inOut' }, 0.5);
-            timeline.current.to(images[counter], { opacity: 1, duration: 1, ease: 'inOut' }, 0.5);
-            timeline.current.to(status[counter], { right: '0%', duration: 2, ease: 'none' }, 2.5);
-            timeline.current.to(status[counter], { left: '100%', duration: 2, ease: 'none ' }, 4.5);
-            timeline.current.to(images[counter], { opacity: 0, duration: 0.5, ease: 'inOut' }, 6);
-        }, sliderRef );
-        return () => context.revert();
     };
 
-
-
     useEffect(() => {
-        playSlide();
-    }, [counter])
+			const context = gsap.context(() => {
+					const status = gsap.utils.toArray('.box .progress__status');
+					const boxes = gsap.utils.toArray('.box');
+					console.log(boxes);
+					const images = gsap.utils.toArray('.preview .preview__image');
+					const labels = gsap.utils.toArray('.box .box__label');
+					const titles = gsap.utils.toArray('.box .box__title');
+					timeline.current = gsap.timeline({ onComplete: updateCounter });
+					timeline.current.to(status, { right: '100%', left: '0%', duration: 0, ease: 'inOut' }, 0);
+					timeline.current.to(labels, { opacity: 0.2, duration: 2, ease: 'inOut' }, 0.5);
+					timeline.current.to(boxes, { zIndex: 0, duration: 0, ease: 'inOut' }, 0);
+					timeline.current.to(boxes[counter], { zIndex: 5, duration: 0, ease: 'inOut' }, 0);
+					timeline.current.to(titles, { opacity: 0.2, duration: 2, ease: 'inOut' }, 0.5);
+					timeline.current.to(images, { opacity: 0, duration: 2, ease: 'inOut' }, 0.5);
+					timeline.current.to(labels[counter], { opacity: 1, duration: 2, ease: 'inOut' }, 2.5);
+					timeline.current.to(titles[counter], { opacity: 1, duration: 2, ease: 'inOut' }, 2.5);
+					timeline.current.to(images[counter], { opacity: 1, duration: 2, ease: 'inOut' }, 2.5);
+					timeline.current.to(status[counter], { opacity: 1, duration: 2, ease: 'none ' }, 2.5);
+					timeline.current.to(status[counter], { right: '0%', duration: 6, ease: 'none' }, 2.5);
+					timeline.current.to(status[counter], { opacity: 0, duration: 2, ease: 'inOut ' }, 10);
+					timeline.current.to(images[counter], { opacity: 0, duration: 2, ease: 'inOut' }, 10);
+					timeline.current.to(labels[counter], { opacity: 0.2, duration: 2, ease: 'inOut' }, 10);
+					timeline.current.to(titles[counter], { opacity: 0.2, duration: 2, ease: 'inOut' }, 10);
+			}, sliderRef );
+			return () => context.revert();
+    }, [counter]);
     
     return (
         <div className={`${ className } highlight-slider`} ref={ sliderRef }>
@@ -52,14 +54,14 @@ const HighlightSlider = ({ className, items }) => {
             </div>
             <div className="highlight-slider__projects projects">
                 { items.map((item) => (
-                    <div className="projects__box box" key={ item.id }>
+                    <Link className="projects__box box" key={ item.id } href={`/projects/${ item.id }`}>
                         <div className="box__progress progress">
                             <div className="progress__background"></div>
                             <div className="progress__status"></div>
                         </div>
                         <Heading className="box__label" level="h3">{ item.attributes.category }</Heading>
                         <Heading className="box__title" level="h5" >{ item.attributes.heading }</Heading>
-                    </div>
+                    </Link>
                 ))}
             </div>
         </div>
