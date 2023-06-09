53f3cca0810b7422e1cc8f2a24a58ba3a824a7c5
jeromehaas <hello@jeromehaas.ch>
2023-06-09 10:54:04 +0200

    Setup: plausible tracking

--- frontend/pages/_app.jsx
+++ frontend/pages/_app.jsx
@@ -1,15 +1,18 @@
 import '../styles/main.scss';
 import ProtectedRoutes from 'utils/protected-routes';
 import { AuthProvider } from 'contexts/auth.jsx';
+import PlausibleProvider from 'next-plausible';
 
 const App = ({ Component, pageProps }) => {
 
 	return (
-		<AuthProvider>
-			<ProtectedRoutes>
-				<Component { ...pageProps } />
-			</ProtectedRoutes>
-		</AuthProvider>
+		<PlausibleProvider domain="samirahaas.ch">
+			<AuthProvider>
+				<ProtectedRoutes>
+					<Component { ...pageProps } />
+				</ProtectedRoutes>
+			</AuthProvider>
+		</PlausibleProvider>
 	);
 
 };
