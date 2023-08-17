81d067e42fac5cb0cf4b1e43aa775aa317f2d8f8
jeromehaas <hello@jeromehaas.ch>
2023-08-17 19:24:55 +0200

    Refactor: remove priority prop for images

--- frontend/components/02-molecules/impressions/impressions.jsx
+++ frontend/components/02-molecules/impressions/impressions.jsx
@@ -2,24 +2,15 @@ import Picture from 'components/01-atoms/picture/picture';
 
 const Impressions = ({ className, items }) => {
 
-	const getLength = (images) => {
-		switch (images.length) {
-		case 1: return 'one';
-		case 2: return 'two';
-		case 3: return 'three';
-		default: return 'one';
-		};
-	};
-
 	return (
 		<div className={ `${ className } impressions` }>
 			<div className="impressions__inner">
 				{ items.map((item, index) => {
 					return (
-						<div className={ `impressions__item item item--${ getLength(item.group.data) }` } key={ item.id }>
+						<div className={ `impressions__item item` } key={ item.id }>
 							{ item.group.data.map((image) => {
 								return (
-									<Picture className="item__image animation--fade-in" src={ image.attributes.url } key={ Math.random() } quality={ 100 } alt="Image" width={ image.attributes.width } height={ image.attributes.height } priority={ index <= 1 } />
+									<Picture className="item__image animation--fade-in" src={ image.attributes.url } quality={ 100 } alt="Image" width={ image.attributes.width } height={ image.attributes.height } priority={ false } />
 								);
 							})}
 						</div>
