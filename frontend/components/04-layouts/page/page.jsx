c836f02e89399cda96a11600a921d3267306d73d
jeromehaas <hello@jeromehaas.ch>
2023-09-10 12:27:11 +0200

    Setup: page transition

--- frontend/components/04-layouts/page/page.jsx
+++ frontend/components/04-layouts/page/page.jsx
@@ -4,14 +4,33 @@ import Navigation from 'components/03-organisms/navigation/navigation';
 import Jumper from 'components/03-organisms/jumper/jumper';
 import Footer from 'components/03-organisms/footer/footer';
 import Cta from 'components/03-organisms/cta/cta';
+import Main from 'components/03-organisms/main/main';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import gsap from 'gsap';
 
 const Page = ({ className, children, meta }) => {
+
+	const router = useRouter();
+
+	const handleRouteChangeStart = () => {
+		gsap.to(['.main', '.cta', '.footer', '.jumper'], { opacity: 0, duration: 0.3 });
+	};
+
+	const handleRouteChangeEnd = () => {
+		gsap.to(['.main', '.cta', '.footer', '.jumper'], { opacity: 1, duration: 0.3 });
+	};
+
+	useEffect(() => {
+		router.events && router.events.on('routeChangeStart', handleRouteChangeStart);
+		router.events && router.events.on('routeChangeComplete', handleRouteChangeEnd);
+	}, []);
 
 	return (
 		<div className={ `${ className } page` }>
 			<Head meta={ meta } />
 			<Navigation />
-			{ children }
+			<Main content={ children } />
 			<Cta />
 			<Jumper />
 			<Footer />
