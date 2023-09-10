2dd667a0b4e46b729fc38910e6af534b73221385
jeromehaas <hello@jeromehaas.ch>
2023-09-10 11:31:40 +0200

    Fix: flicker on route-changes on project-pages

--- frontend/components/02-molecules/impressions/impressions.jsx
+++ frontend/components/02-molecules/impressions/impressions.jsx
@@ -5,17 +5,13 @@ const Impressions = ({ className, items }) => {
 	return (
 		<div className={ `${ className } impressions` }>
 			<div className="impressions__inner">
-				{ items.map((item, index) => {
-					return (
-						<div className={ `impressions__item item` } key={ item.id }>
-							{ item.group.data.map((image) => {
-								return (
-									<Picture className="item__image animation--fade-in" src={ image.attributes.url } quality={ 100 } alt="Image" width={ image.attributes.width } height={ image.attributes.height } priority={ false } />
-								);
-							})}
-						</div>
-					);
-				})}
+				{ items.map((item) => (
+					<div className="impressions__item item" key={ item.id }>
+						{ item.group.data.map((image, index) => (
+							<Picture className="item__image animation--fade-in" src={ image.attributes.url } key={ `${ item.id }-${ index } ` } quality={ 100 } alt="Image" width={ image.attributes.width } height={ image.attributes.height } priority={ false } />
+						))}
+					</div>
+				))}
 			</div>
 		</div>
 	);
